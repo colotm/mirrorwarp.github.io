739cb0297198dd1e729b71b53c46edb95468f164
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-30 07:25:09 -0400

    Update index.js

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -3,7 +3,7 @@ import projectData from './project-data';
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./mini-gaia.svg';
+import costume1 from '!raw-loader!./penguin.svg';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
