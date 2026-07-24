14f67adf89bcd86e1a79acbb29ee5926c1896939
Titima <colonruizanabel@gmail.com>
2026-07-23 23:31:16 -0400

    Refactor icon imports and update Makey Makey extension
    Comment out unused icon imports and adjust Makey Makey extension details.

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import {APP_NAME} from '../../brand';
 
-import musicIconURL from './music/music.png';
-import musicInsetIconURL from './music/music-small.svg';
+// import musicIconURL from './music/music.png';
+// import musicInsetIconURL from './music/music-small.svg';
 
 import penIconURL from './pen/pen.png';
 import penInsetIconURL from './pen/pen-small.svg';
@@ -12,22 +12,22 @@ import penInsetIconURL from './pen/pen-small.svg';
 import videoSensingIconURL from './videoSensing/video-sensing.png';
 import videoSensingInsetIconURL from './videoSensing/video-sensing-small.svg';
 
-import text2speechIconURL from './text2speech/text2speech.png';
-import text2speechInsetIconURL from './text2speech/text2speech-small.svg';
+//import text2speechIconURL from './text2speech/text2speech.png';
+//import text2speechInsetIconURL from './text2speech/text2speech-small.svg';
 
-import translateIconURL from './translate/translate.png';
-import translateInsetIconURL from './translate/translate-small.png';
+//import translateIconURL from './translate/translate.png';
+//import translateInsetIconURL from './translate/translate-small.png';
 
-import makeymakeyIconURL from './makeymakey/makeymakey.png';
-import makeymakeyInsetIconURL from './makeymakey/makeymakey-small.svg';
+//import makeymakeyIconURL from './makeymakey/makeymakey.png';
+//import makeymakeyInsetIconURL from './makeymakey/makeymakey-small.svg';
 
 import animatedTextIconURL from './penguinmod/extensions/text extension.png';
 import animatedTextInsetIconURL from './penguinmod/extensions/text extension small.svg';
 
-import microbitIconURL from './microbit/microbit.png';
-import microbitInsetIconURL from './microbit/microbit-small.svg';
-import microbitConnectionIconURL from './microbit/microbit-illustration.svg';
-import microbitConnectionSmallIconURL from './microbit/microbit-small.svg';
+//import microbitIconURL from './microbit/microbit.png';
+//import microbitInsetIconURL from './microbit/microbit-small.svg';
+//import microbitConnectionIconURL from './microbit/microbit-illustration.svg';
+/import microbitConnectionSmallIconURL from './microbit/microbit-small.svg';
 
 import ev3IconURL from './ev3/ev3.png';
 import ev3InsetIconURL from './ev3/ev3-small.svg';
@@ -75,6 +75,7 @@ import jwXmlExtensionIcon from './penguinmod/extensions/xml.png';
 
 import jwStructsExtensionIcon from './penguinmod/extensions/ooplogo.png';
 
+/*
 import jwArrayExtensionThumb from './penguinmod/extensions/jwArray.svg';
 import jwTargetsExtensionThumb from './penguinmod/extensions/jwTargets.svg';
 import jwNumExtensionThumb from './penguinmod/extensions/jwNum.svg';
@@ -82,6 +83,7 @@ import jwColorExtensionThumb from './penguinmod/extensions/jwColor.svg';
 import jwVectorExtensionThumb from './penguinmod/extensions/jwVector.svg';
 import jwLambdaExtensionThumb from './penguinmod/extensions/jwLambda.svg';
 import jwScopeExtensionThumb from './penguinmod/extensions/jwScope.svg';
+*/
 
 import iygPerlinNoiseExtensionIcon from './penguinmod/extensions/perlinnoisebanner.png';
 
@@ -201,7 +203,7 @@ const menuItems = [
     //     ),
     //     extensionId: 'music',
     //     iconURL: musicIconURL,
-    //     insetIconURL: musicInsetIconURL,
+    //   insetIconURL: musicInsetIconURL,
     //     customInsetColor: '#CF63CF',
     //     tags: ['scratch', 'noisemaker'],
     //     description: (
@@ -270,7 +272,7 @@ const menuItems = [
         name: 'Face Sensing',
         extensionId: 'https://extensions.turbowarp.org/lab/face-sensing.js',
         iconURL: faceSensingIconURL,
-        tags: ['scratch'],
+//        tags: ['scratch'],
         description: 'Sense faces with the camera.',
         featured: true
     },
@@ -321,25 +323,24 @@ const menuItems = [
     //         />
     //     ),
     //     featured: true,
-    //     internetConnectionRequired: true
+    //     internetConnectionRequire: true
     // },
-    // {
-    //     name: 'Makey Makey',
-    //     extensionId: 'makeymakey',
-    //     collaborator: 'JoyLabz',
-    //     iconURL: makeymakeyIconURL,
-    //     insetIconURL: makeymakeyInsetIconURL,
+ {
+       name: 'Makey Makey',
+       extensionId: 'makeymakey',
+     collaborator: 'JoyLabz',
+      iconURL: makeymakeyIconURL,
+       insetIconURL: makeymakeyInsetIconURL,
     //     customInsetColor: '#E64D00',
-    //     tags: ['scratch', 'hardware'],
-    //     description: (
-    //         <FormattedMessage
-    //             defaultMessage="Make anything into a key."
+     description: (
+        <FormattedMessage
+               defaultMessage="Make anything into a key."
     //             description="Description for the 'Makey Makey' extension"
-    //             id="gui.extension.makeymakey.description"
-    //         />
-    //     ),
-    //     featured: false
-    // },
+                 id="gui.extension.makeymakey.description"
+           />
+         ),
+    featured: false
+     },
     {
         name: 'Project Interfaces',
         extensionId: '../../../../../',
@@ -884,6 +885,7 @@ const menuItems = [
         extensionId: 'https://extensions.turbowarp.org/cursor.js',
         tags: ['turbowarp'],
         insetIconURL: turbowarpIcon,
+        // anymore interpolation
         iconURL: 'https://extensions.turbowarp.org/images/cursor.png',
         description: (
             <FormattedMessage
